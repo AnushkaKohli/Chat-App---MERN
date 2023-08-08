c544312f830bd8a48b97e9bcbec7604e4ada76d1
AnushkaKohli <102284552+AnushkaKohli@users.noreply.github.com>
2023-08-08 22:19:27 +0530

    signup page initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ChatPage from "./Pages/ChatPage";
+import SignupPage from "./Pages/SignupPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" exact element={<HomePage />} />
+        <Route path="/signup" exact element={<SignupPage />} />
         <Route path="/chats" exact element={<ChatPage />} />
       </Routes>
     </>
